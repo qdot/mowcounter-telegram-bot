b089a5254ddea089e0f31ea10f1d93f25f7f92c9
Kyle Machulis <kyle@nonpolynomial.com>
2016-05-29 13:28:08 -0700

    Still renaming stuff

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-watchmedo auto-restart --pattern="*.py" -R -- python3.4 metafetish_bot.py --token=token.txt --dbdir=dbs
+watchmedo auto-restart --pattern="*.py" -R -- python3.4 mowcounter_bot.py --token=token.txt --dbdir=dbs
